e96fd8eeb2803d61d3368cce14782d448e9cf3cf
Andrés Medinilla <diggs.papu@gmail.com>
2024-02-26 15:39:31 -0600

    udpate:ReadingMatrix

--- Implementations/main.py
+++ Implementations/main.py
@@ -1,7 +1,7 @@
 import openpyxl
 from Algorithms import *
 import time
-
+from Labyrinth import *
 def thisGraphComparator(newNode:tuple,node:tuple):
     return newNode[1]>=node[1]
 def thisHeuristicComparator(newNode:tuple, node:tuple):
@@ -90,5 +90,44 @@ def main():
     print("Elapsed time:", elapsed_time, " s")
 # Using the special variable
 # __name__
-if __name__=="__main__":
-    main()
+# if __name__=="__main__":
+#     main()
+matrix = generateBasicLabyrinth()
+graph = {}
+entranceNode = None
+objectiveNode = None
+for i in range(0,len(matrix)): 
+    for j in range(0,len(matrix[0])):    
+        # Si es pared no se aniadira como nodo 
+        if matrix[i][j]!=0:
+            # Nombre del nodo
+            node_name = str(i)+"-"+str(j)
+            # Se agarra la direccion derecha, izquierda, arriba y abajo
+            directions = []
+            # No se puede mover a la izquierda si es el primer nodo de cada fila
+            if j!=0 and matrix[i][j-1]!=0:
+                directions.append((str(i)+"-"+str(j-1),1))
+            # No se puede mover a la derecha si es el ultimo nodo de cada fila
+            if j!=len(matrix[0])-1 and matrix[i][j+1]!=0:
+                directions.append((str(i)+"-"+str(j+1),1))
+            # No se puede mover hacia arriba si es alguno de los nodos de la fila 0
+            if i!=0 and matrix[i-1][j]!=0:
+                directions.append((str(i-1)+"-"+str(j),1))
+            # No se puede mover hacia abajo si es alguno de los nodos de la fila 64    
+            if i!=len(matrix)-1 and matrix[i+1][j]!=0:
+                directions.append((str(i+1)+"-"+str(j),1))
+            graph[node_name]=[]
+            for direction in directions:
+                graph[node_name].append(direction)
+        if matrix[i][j]==3:
+            finalNode = str(i)+"-"+str(j)
+        if matrix[i][j]==2:
+            entranceNode = str(i)+"-"+str(j)
+            
+print("\n\nBreadth First Search with a final node", end=":\n")
+start_time = time.time()
+BreadthFirstSearch(graph=graph, init_node=entranceNode, goal_node=finalNode)
+elapsed_time = -start_time + time.time()
+print("Elapsed time:", elapsed_time, " s")
+    
+        
