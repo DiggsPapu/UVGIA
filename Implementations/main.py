f014f9fea88222dc9015cb1d5e38f253646f9f6f
Andrés Medinilla <diggs.papu@gmail.com>
2024-02-26 17:56:00 -0600

    add:TxtMaze
    Se añadio el texto con el tipo de maze que se evaluará, así mismo, se hizo pruebas y funcionan varios de los algoritmos, cómo mínimo el breadth first, y el depth first search sí funcionan, falta hacer el comparator para un uniform search

--- Implementations/main.py
+++ Implementations/main.py
@@ -1,4 +1,3 @@
-import openpyxl
 from Algorithms import *
 import time
 from Labyrinth import *
@@ -9,30 +8,36 @@ def thisHeuristicComparator(newNode:tuple, node:tuple):
 def thisFComparator(newNode:tuple, node:tuple):
     return newNode[4]>=node[4]
 def main():
-    
-    matrix = generateBasicLabyrinth()
+    matrix_path = "./test_maze.txt"
+    matrix = []
+    with open(matrix_path, 'r') as file:
+        # Read the contents of the file
+        file_contents = file.read()
+        for  line in file_contents.split('\n'):
+            matrix.append([int(item) for item in list(line)])
+    # matrix = generateBasicLabyrinth()
     graph = {}
     entranceNode = None
-    objectiveNode = None
+    finalNode = None
     for i in range(0,len(matrix)): 
         for j in range(0,len(matrix[0])):    
             # Si es pared no se aniadira como nodo 
-            if matrix[i][j]!=0:
+            if matrix[i][j]>0:
                 # Nombre del nodo
                 node_name = str(i)+"-"+str(j)
                 # Se agarra la direccion derecha, izquierda, arriba y abajo
                 directions = []
                 # No se puede mover a la izquierda si es el primer nodo de cada fila
-                if j!=0 and matrix[i][j-1]!=0:
+                if j!=0 and matrix[i][j-1]>=1:
                     directions.append((str(i)+"-"+str(j-1),1))
                 # No se puede mover a la derecha si es el ultimo nodo de cada fila
                 if j!=len(matrix[0])-1 and matrix[i][j+1]!=0:
                     directions.append((str(i)+"-"+str(j+1),1))
                 # No se puede mover hacia arriba si es alguno de los nodos de la fila 0
-                if i!=0 and matrix[i-1][j]!=0:
+                if i!=0 and matrix[i-1][j]>=1:
                     directions.append((str(i-1)+"-"+str(j),1))
                 # No se puede mover hacia abajo si es alguno de los nodos de la fila 64    
-                if i!=len(matrix)-1 and matrix[i+1][j]!=0:
+                if i!=len(matrix)-1 and matrix[i+1][j]>=1:
                     directions.append((str(i+1)+"-"+str(j),1))
                 graph[node_name]=[]
                 for direction in directions:
@@ -64,16 +69,16 @@ def main():
 
     print("\n\nDepth First Search with a final node", end=":\n")
     start_time = time.time()
-    DepthFirstSearch(graph,  "Warm-up activities", "Stretching")
+    DepthFirstSearch(graph,  entranceNode, finalNode)
     elapsed_time = -start_time + time.time()
     print("Elapsed time:", elapsed_time, " s")
     
     
-    print("\n\nUniform Cost Search with a final node", end=":\n")
-    start_time = time.time()
-    UniformCostSearch(thisGraphComparator, graph,entranceNode,finalNode)
-    elapsed_time = -start_time + time.time()
-    print("Elapsed time:", elapsed_time, " s")
+    # print("\n\nUniform Cost Search with a final node", end=":\n")
+    # start_time = time.time()
+    # UniformCostSearch(thisGraphComparator, graph,entranceNode,finalNode)
+    # elapsed_time = -start_time + time.time()
+    # print("Elapsed time:", elapsed_time, " s")
     
     # print("\n\nGreedy Best First Search with a final node", end=":\n")
     # start_time = time.time()
